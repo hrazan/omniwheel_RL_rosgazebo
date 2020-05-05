39f41828552a67f871aec165111371ae5068d8af
hrazan <hafiyanda.razan@gmail.com>
2020-05-05 18:21:02 +0900

    actor_critic v0.3

--- actor_critic.py
+++ actor_critic.py
@@ -1,184 +1,145 @@
 import random
 
 import numpy as np
-from keras import Sequential, optimizers
-from keras.layers import Dense, Activation, Dropout
-from keras.models import load_model
-from keras.optimizers import Adam
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense, Add, Input, Concatenate
+import tensorflow.keras.backend as K
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+from collections import deque
+import memory 
 
-import memory
+# Hyperparameters
+REPLAY_MEMORY_SIZE = 100000
+MINIBATCH_SIZE = 64
+DISCOUNT = 0.99
 
+class Actor(object):
+    def __init__(self, sess, action_dim, observation_dim):
+        # setting our created session as default session
+        self.sess = sess
+        K.set_session(sess)
+        self.action_dim = action_dim
+        self.observation_dim = observation_dim
+        self.state_input, self.output, self.model = self.create_model()
+        model_weights = self.model.trainable_weights
+        # Placeholder for critic gradients with respect to action_input.
+        self.actor_critic_grads = tf.placeholder(tf.float32, [None, action_dim])
+        # Adding small number inside log to avoid log(0) = -infinity
+        log_prob = tf.math.log(self.output + 10e-10)
+        # Multiply log by -1 to convert the optimization problem as minimization problem.
+        # This step is essential because apply_gradients always do minimization.
+        neg_log_prob = tf.multiply(log_prob, -1)
+        # Calulate and update the weights of the model to optimize the actor
+        self.actor_grads = tf.gradients(neg_log_prob, model_weights, self.actor_critic_grads)
+        grads = zip(self.actor_grads, model_weights)
+        self.optimize = tf.train.AdamOptimizer(0.001).apply_gradients(grads)
 
-class DeepQ:
-    """
-    DQN abstraction.
+    def create_model(self):
+        state_input = Input(shape=self.observation_dim)
+        state_h1 = Dense(128, activation='relu')(state_input)
+        state_h2 = Dense(128, activation='relu')(state_h1)
+        state_h3 = Dense(128, activation='relu')(state_h2)
+        output = Dense(self.action_dim, activation='softmax')(state_h2)
+        model = Model(inputs=state_input, outputs=output)
+        adam = Adam(lr=0.001)
+        model.compile(loss='categorical_crossentropy', optimizer=adam)
+        return state_input, output, model
 
-    As a quick reminder:
-        traditional Q-learning:
-            Q(s, a) += alpha * (reward(s,a) + gamma * max(Q(s') - Q(s,a))
-        DQN:
-            target = reward(s,a) + gamma * max(Q(s')
+    def train(self, critic_gradients_val, X_states):
+        self.sess.run(self.optimize, feed_dict={self.state_input:X_states, self.actor_critic_grads:critic_gradients_val})
 
-    """
-    def __init__(self, inputs, outputs, memorySize, discountFactor, learningRate, learnStart):
-        """
-        Parameters:
-            - inputs: input size
-            - outputs: output size
-            - memorySize: size of the memory that will store each state
-            - discountFactor: the discount factor (gamma)
-            - learningRate: learning rate
-            - learnStart: steps to happen before for learning. Set to 128
-        """
-        self.input_size = inputs
-        self.output_size = outputs
-        self.memory = memory.Memory(memorySize)
-        self.discountFactor = discountFactor
-        self.learnStart = learnStart
-        self.learningRate = learningRate
+class Critic(object):
+    def __init__(self, sess, action_dim, observation_dim):
+        # setting our created session as default session
+        K.set_session(sess)
+        self.sess = sess
+        self.action_dim = action_dim
+        self.observation_dim = observation_dim
+        self.state_input, self.action_input, self.output, self.model = self.create_model()
+        self.critic_gradients = tf.gradients(self.output, self.action_input)
 
-    def initNetworks(self, hiddenLayers):
-        actor_model, critic_model = self.createModel(self.input_size, self.output_size, hiddenLayers, "relu", self.learningRate)
-        self.actor_model = actor_model
-        self.critic_model = critic_model
-
-        target_actor_model, target_critic_model = self.createModel(self.input_size, self.output_size, hiddenLayers, "relu", self.learningRate)
-        self.target_actor_model = target_actor_model
-        self.target_critic_model = target_critic_model
-
-	# ========================================================================= #
-	#                              Model Definitions                            #
-	# ========================================================================= #
-
-    def createModel(self, inputs, outputs, hiddenLayers, activationType, learningRate):
-		adam  = Adam(lr=learningRate)        
+    def create_model(self):
+        state_input = Input(shape=self.observation_dim)
+        state_h1 = Dense(128, activation='relu')(state_input)
         
-        # Actor Network Model
-        state_input = Input(shape=(inputs, ))
-		h1 = Dense(128, activation=activationType)(state_input)
-		h2 = Dense(128, activation=activationType)(h1)
-		h3 = Dense(128, activation=activationType)(h2)
-		output = Dense(outputs, activation=activationType)(h3)
-		
-		actor_model = Model(inputs=state_input, outputs=output)
-		actor_model.compile(loss="mse", optimizer=adam)
-		actor_model.summary()
-		
-		# Critic Network Model
-		state_input = Input(shape=self.env.observation_space.shape)
-		state_h1 = Dense(128, activation=activationType)(state_input)
-		
-		action_input = Input(shape=self.env.action_space.shape)
-		merged = Concatenate()([state_h1, action_input])
-		merged_h1 = Dense(128, activation=activationType)(merged)
-		merged_h2 = Dense(128)(merged_h1)
-		output = Dense(1, activation=activationType)(merged_h1)
-		critic_model  = Model(inputs=[state_input,action_input], outputs=output)
-		critic_model.compile(loss="mse", optimizer=adam)
-		critic_model.summary()
+        action_input = Input(shape=(self.action_dim,))
+        state_action = Concatenate()([state_h1, action_input])
+        state_action_h1 = Dense(128, activation='relu')(state_action)
+        state_action_h2 = Dense(128, activation='relu')(state_action_h1)        
+        output = Dense(1, activation='linear')(state_action_h1)
+
+        model = Model(inputs=[state_input, action_input], outputs=output)
+        model.compile(loss="mse", optimizer=Adam(lr=0.005))
+        return state_input, action_input, output, model
+
+    def get_critic_gradients(self, X_states, X_actions):
+        # critic gradients with respect to action_input to feed in the weight updation of actor
+        critic_gradients_val = self.sess.run(self.critic_gradients, feed_dict={self.state_input:X_states, self.action_input:X_actions})
+        return critic_gradients_val[0]
+
+class ActorCritic:
+    def __init__(self, env, sess):
+        # Environment details
+        self.env = env
+        self.action_dim = env.action_space.shape[0]
+        self.observation_dim = env.observation_space.shape[0]
         
-        return actor_model, critic_model
+        # Actor model to take actions 
+        # state -> action
+        self.actor = Actor(sess, self.action_dim, self.observation_dim)
+        # Critic model to evaluate the action taken by the actor
+        # state + action -> Expected reward to be achieved by taking action in the state.
+        self.critic = Critic(sess, self.action_dim, self.observation_dim)
+        
+        # Replay memory to store experiences of the model with the environment
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+        
+    def train(self):
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
 
-    def printNetwork(self):
-        i = 0
-        for layer in self.model.layers:
-            weights = layer.get_weights()
-            print("layer ",i,": ",weights)
-            i += 1
+        X_states = []
+        X_actions = []
+        y = []
+        for sample in minibatch:
+            cur_state, cur_action, reward, next_state, done = sample
+            next_actions = self.actor.model.predict(np.expand_dims(next_state, axis=0))
+            if done:
+                # If episode ends means we have lost the game so we give -ve reward
+                # Q(st, at) = -reward
+                reward = -reward
+            else:
+                # Q(st, at) = reward + DISCOUNT * Q(s(t+1), a(t+1))
+                next_reward = self.critic.model.predict([np.expand_dims(next_state, axis=0), next_actions])[0][0]
+                reward = reward + DISCOUNT * next_reward
 
-    def backupNetwork(self, model, backup):
-        weightMatrix = []
-        for layer in model.layers:
-            weights = layer.get_weights()
-            weightMatrix.append(weights)
-        i = 0
-        for layer in backup.layers:
-            weights = weightMatrix[i]
-            layer.set_weights(weights)
-            i += 1
+            X_states.append(cur_state)
+            X_actions.append(cur_action)
+            y.append(reward)
 
-    def updateTargetNetwork(self):
-        self.backupNetwork(self.model, self.targetModel)
+        X_states = np.array(X_states)
+        X_actions = np.array(X_actions)
+        X = [X_states, X_actions]
+        y = np.array(y)
+        y = np.expand_dims(y, axis=1)
+        # Train critic model
+        self.critic.model.fit(X, y, batch_size=MINIBATCH_SIZE, verbose = 0)
 
-    # predict Q values for all the actions
-    def getQValues(self, state):
-        predicted = self.model.predict(state.reshape(1,len(state)))
-        #print(state)
-        #print("predicted: ",predicted)
-        return predicted[0]
+        # Get the actions for the cur_states from the minibatch.
+        # We are doing this because now actor may have learnt more optimal actions for given states
+        # as Actor is constantly learning and we are picking the states from the previous experiences.
+        X_actions_new = []
+        for sample in minibatch:
+            X_actions_new.append(self.actor.model.predict(np.expand_dims(sample[0], axis=0))[0])
+        X_actions_new = np.array(X_actions_new)
 
-    def getTargetQValues(self, state):
-        #predicted = self.targetModel.predict(state.reshape(1,len(state)))
-        predicted = self.targetModel.predict(state.reshape(1,len(state)))
+        # grad(J(actor_weights)) = sum[ grad(log(pi(at | st, actor_weights)) * grad(critic_output, action_input), actor_weights) ]
+        critic_gradients_val = self.critic.get_critic_gradients(X_states, X_actions)
+        self.actor.train(critic_gradients_val, X_states)
+    
+    def act(self, cur_state, epsilon):
+        if np.random.random() < epsilon:
+            return self.env.action_space.sample()
+        return self.actor.predict(cur_state)
 
-        return predicted[0]
 
-    def getMaxQ(self, qValues):
-        return np.max(qValues)
-
-    def getMaxIndex(self, qValues):
-        return np.argmax(qValues)
-
-    # calculate the target function
-    def calculateTarget(self, qValuesNewState, reward, isFinal):
-        """
-        target = reward(s,a) + gamma * max(Q(s')
-        """
-        if isFinal:
-            return reward
-        else :
-            return reward + self.discountFactor * self.getMaxQ(qValuesNewState)
-
-    # select the action with the highest Q value
-    def selectAction(self, qValues, explorationRate):
-        rand = random.random()
-        if rand < explorationRate :
-            action = np.random.randint(0, self.output_size)
-            #print('random')
-        else :
-            action = self.getMaxIndex(qValues)
-            #print(qValues, action)
-        return action
-
-    def addMemory(self, state, action, reward, newState, isFinal):
-        self.memory.addMemory(state, action, reward, newState, isFinal)
-
-    def learnOnLastState(self):
-        if self.memory.getCurrentSize() >= 1:
-            return self.memory.getMemory(self.memory.getCurrentSize() - 1)
-
-    def learnOnMiniBatch(self, miniBatchSize, useTargetNetwork=True):
-        # Do not learn until we've got self.learnStart samples
-        if self.memory.getCurrentSize() > self.learnStart:
-            # learn in batches of 128
-            miniBatch = self.memory.getMiniBatch(miniBatchSize)
-            X_batch = np.empty((0,self.input_size), dtype = np.float64)
-            Y_batch = np.empty((0,self.output_size), dtype = np.float64)
-            for sample in miniBatch:
-                isFinal = sample['isFinal']
-                state = sample['state']
-                action = sample['action']
-                reward = sample['reward']
-                newState = sample['newState']
-
-                qValues = self.getQValues(state)
-                if useTargetNetwork:
-                    qValuesNewState = self.getTargetQValues(newState)
-                else :
-                    qValuesNewState = self.getQValues(newState)
-                targetValue = self.calculateTarget(qValuesNewState, reward, isFinal)
-
-                X_batch = np.append(X_batch, np.array([state.copy()]), axis=0)
-                Y_sample = qValues.copy()
-                Y_sample[action] = targetValue
-                Y_batch = np.append(Y_batch, np.array([Y_sample]), axis=0)
-                if isFinal:
-                    X_batch = np.append(X_batch, np.array([newState.copy()]), axis=0)
-                    Y_batch = np.append(Y_batch, np.array([[reward]*self.output_size]), axis=0)
-            self.model.fit(X_batch, Y_batch, batch_size = len(miniBatch), epochs=1, verbose = 0)
-
-    def saveModel(self, path):
-        self.model.save(path)
-
-    def loadWeights(self, path):
-        self.model.set_weights(load_model(path).get_weights())
--- project_ac.py
+++ project_ac.py
@@ -13,6 +13,15 @@ import csv
 import numpy as np
 import tensorflow.compat.v1 as tf
 
+# Hyperparameters
+MINIMUM_REPLAY_MEMORY = 1000
+EPSILON = 1
+EPSILON_DECAY = 0.999
+MIN_EPSILON = 0.001
+DISCOUNT = 0.99
+EPISODES = 2000
+STEPS = 100
+
 def detect_monitor_files(training_dir):
     return [os.path.join(training_dir, f) for f in os.listdir(training_dir) if f.startswith('openaigym')]
 
@@ -41,126 +50,61 @@ if __name__ == '__main__':
     weights_path = resume_path + '.h5'
     monitor_path = resume_path
     params_json  = resume_path + '.json'
-
-    if not continue_execution:
-        #Each time we take a sample and update our weights it is called a mini-batch.
-        #Each time we run through the entire dataset, it's called an epoch.
-        #PARAMETER LIST
-        episodes = 2000
-        steps = 100
-        updateTargetNetwork = 1000
-        epsilon = 1
-        epsilon_decay = 0.97
-        batch_size = 64
-        discountFactor = 0.99
-        memorySize = 1000000
-        #actor_net_structure = [1000,1000,1000]
-        #critic_net_structure = [1000,1000,1000]
-        #actor_learningRate = 0.00001
-        #critic_learningRate = 0.00001
-        learningRate = 0.00001
-        current_episode = 0
-
-        actor_critic = ac.ActorCritic(env, memorySize, discountFactor, learningRate, batch_size)
-    else:
-        #Load weights, monitor info and parameter info.
-        #ADD TRY CATCH fro this else
-        with open(params_json) as outfile:
-            d = json.load(outfile)
-            epochs = d.get('episodes')
-            steps = d.get('steps')
-            updateTargetNetwork = d.get('updateTargetNetwork')
-            epsilon = d.get('epsilon')
-            epsilon_decay = d.get('epsilon_decay')
-            batch_size = d.get('batch_size')
-            learningRate = d.get('learningRate')
-            discountFactor = d.get('discountFactor')
-            memorySize = d.get('memorySize')
-            #network_inputs = d.get('network_inputs')
-            #network_outputs = d.get('network_outputs')
-            #network_structure = d.get('network_structure')
-            current_episode = d.get('current_episode')
-
-        actor_critic = ac.ActorCritic(env, sess, memorySize, discountFactor, learningRate, batch_size)
-
-        actor_critic.loadWeights(weights_path)
-
-        clear_monitor_files(outdir)
-        copy_tree(monitor_path,outdir)
-
-    env._max_episode_steps = steps # env returns done after _max_episode_steps
+    
+    env._max_episode_steps = STEPS # env returns done after _max_episode_steps
     env = gym.wrappers.Monitor(env, outdir,force=not continue_execution, resume=continue_execution)
+
+    actor_critic = ac.ActorCritic(env, sess)
 
     stepCounter = 0
     min_distance = 20
+    max_reward = 0
     
     start_time = time.time()
 
-    for eps in xrange(current_episode+1, episodes+1, 1):
-        cur_state = env.reset()
-        if np.any(np.isinf(cur_state)): print('ERROR! 0')
-        cumulated_reward = 0
+    for episode in range(EPISODES):
         done = False
+        cur_state = env.reset()
+        episode_reward = 0
         episode_step = 0
         
-        # run intil env returns done
         while not done:
-            cur_state = cur_state.reshape((1, env.observation_space.shape[0]))
-            if np.any(np.isinf(cur_state)): print('ERROR! 1')
-            #print('state: ' + str(cur_state))
-            action = actor_critic.act(cur_state, epsilon)
-            #print('actor_critic.act(cur_state, epsilon): ' + str(action))
-            action = action.reshape((1, env.action_space.shape[0]))
-            if np.any(np.isnan(action)): print('ERROR! 2')
-            #print('action.reshape((1, env.action_space.shape[0]): ' + str(action))
-
-            new_state, reward, done, _ = env.step(action)
-            new_state = new_state.reshape((1, env.observation_space.shape[0]))
-
-            actor_critic.remember(cur_state, action, reward, new_state, done)
-            actor_critic.train()
+            action = actor_critic.act(cur_state, EPSILON)
             
-            cumulated_reward += reward
+            action = np.array(action, dtype=np.float32)
+            #print(action[0] )
+            next_state, reward, done, _ = env.step(action)
 
-            cur_state = new_state
-            #print(cur_state[0][-1])
-            if cur_state[0][-1] < min_distance:
-                min_distance = cur_state[0][-1]
+            episode_reward += reward
+
+            # Add experience to replay memory
+            actor_critic.replay_memory.append((cur_state, action, reward, next_state, done))
+
+            cur_state = next_state
             
             episode_step += 1
-            stepCounter += 1
-            if stepCounter % updateTargetNetwork == 0:
-                actor_critic.update_target()
-                print ("updating target network")
-        
-        #env.reset_vel()
-        
-        m, s = divmod(int(time.time() - start_time), 60)
-        h, m = divmod(m, 60)
-        
-        print ("EP " + str(eps) + " - " + format(episode_step) + "/" + str(steps) + " steps | e=" + str(round(epsilon, 2)) + " | R: " + str(cumulated_reward) + " | To goal: " + str(min_distance)+ " | Time: %d:%02d:%02d" % (h, m, s))
-        
+
+            if len(actor_critic.replay_memory) < MINIMUM_REPLAY_MEMORY:
+                continue
+
+            actor_critic.train()
+
+            if EPSILON > MIN_EPSILON and len(actor_critic.replay_memory) >= MINIMUM_REPLAY_MEMORY:
+                EPSILON *= EPSILON_DECAY
+                EPSILON = max(EPSILON, MIN_EPSILON)
+
+        #some bookkeeping
         """
-        if (eps)%100==0:
-            #save model weights and monitoring data every 100 epochs.
-            actor_critic.saveModel(path+str(epoch)+'.h5')
-            env._flush()
-            copy_tree(outdir,path+str(epoch))
-            #save simulation parameters.
-            parameter_keys = ['episodes','steps','updateTargetNetwork','epsilon','epsilon_decay', 'batch_size','learningRate','discountFactor','memorySize','current_episode']
-            parameter_values = [episodes, steps, updateTargetNetwork, epsilon, epsilon_decay, batch_size, learningRate, discountFactor, memorySize, eps]
-            parameter_dictionary = dict(zip(parameter_keys, parameter_values))
-            with open(path+str(eps)+'.json', 'w') as outfile:
-                json.dump(parameter_dictionary, outfile)
-		"""
-		
-        with open('/home/katolab/experiment_data/AC_data/reward_ac.csv','a+') as csvRWRD:
+        if(episode_reward > 400 and episode_reward > max_reward):
+            actor.model.save_weights(str(episode_reward)+".h5")
+        max_reward = max(max_reward, episode_reward)
+        """
+        print("Episode:" + str(episode) + " - " + str(episode_step) + "/" + str(STEPS) + " steps |" + " Episodic Reward: " + str(episode_reward) + " | Max Reward Achieved:" + str(max_reward) + " | EPSILON: " + str(EPSILON))
+        
+        with open('/home/katolab/experiment_data/reward.csv','a+') as csvRWRD:
             csvRWRD_writer = csv.writer(csvRWRD,dialect='excel')
-            csvRWRD_writer.writerow([episode_step, cumulated_reward])
+            csvRWRD_writer.writerow([episode_step, episode_reward])
         csvRWRD.close()
-		
-        epsilon *= epsilon_decay
-        epsilon = max (0.05, epsilon)
             
     input()
     env.close()
